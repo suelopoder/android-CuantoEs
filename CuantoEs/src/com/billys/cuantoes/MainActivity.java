10c78ebc89d3f28a4e18b1f513edf723af84f4bd
Diego <diego@diego-HP-Pavilion-dv6-Notebook-PC.(none)>
2012-10-15 00:48:17 -0300

    Agregué el llamado a otra actividad con el botón

--- CuantoEs/src/com/billys/cuantoes/MainActivity.java
+++ CuantoEs/src/com/billys/cuantoes/MainActivity.java
@@ -2,7 +2,10 @@ package com.billys.cuantoes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.romanaButton);
+        button.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v) {
+        		startActivity(new Intent(MainActivity.this, RomanaActivity.class));
+			}
+        });
     }
 
     @Override
