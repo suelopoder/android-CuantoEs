b2f7aa92f52ce919478571b7f44b12207fe1cde7
Diego <diego@diego-HP-Pavilion-dv6-Notebook-PC.(none)>
2012-10-16 00:00:23 -0300

    Agregué un texto y field fijos a la segunda actividad

--- CuantoEs/src/com/billys/cuantoes/MainActivity.java
+++ CuantoEs/src/com/billys/cuantoes/MainActivity.java
@@ -1,11 +1,10 @@
 package com.billys.cuantoes;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,18 +12,15 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        final Button button = (Button) findViewById(R.id.romanaButton);
-        button.setOnClickListener(new View.OnClickListener(){
-        	public void onClick(View v) {
-        		startActivity(new Intent(MainActivity.this, RomanaActivity.class));
-			}
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void showRomana(View view) {
+    	startActivity(new Intent(this, RomanaActivity.class));
     }
 }
--- CuantoEs/src/com/billys/cuantoes/RomanaActivity.java
+++ CuantoEs/src/com/billys/cuantoes/RomanaActivity.java
@@ -1,13 +1,26 @@
 package com.billys.cuantoes;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class RomanaActivity extends Activity {
+public class RomanaActivity extends FragmentActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.romana);
-	}
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            GastoFragment firstFragment = new GastoFragment();
+            getSupportFragmentManager()
+            	.beginTransaction()
+                .add(R.id.fragment_container, firstFragment)
+                .commit();
+        }
+    }
 }
